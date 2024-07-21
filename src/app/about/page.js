93b3005993d5029102ddc56fc39385b45496c9de
Amila Roshan <amilaro89@gmail.com>
2024-07-21 10:18:56 +0530

    initial pages added

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+function About(){
+    return(
+        <>
+        <h2>about page is here..</h2>
+        <p>welcome to bookstore</p>
+        <p>hi there..</p>
+        </>
+    )
+}
+
+export default About
---
+++ src/app/collection/page.js
@@ -0,0 +1,5 @@
+const Collection = () => {
+  return <h2>books collection is here...</h2>;
+};
+
+export default Collection
